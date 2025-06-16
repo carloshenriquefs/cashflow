5b9ca501e05e094fcb64addc171756e3ef2253d8
Carlos Henrique <ichfs9108@gmail.com>
2025-06-16 01:14:28 -0300

    utilizando FluentValidation

--- CashFlow/src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ CashFlow/src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,7 +1,5 @@
-﻿using CashFlow.Communication.Enums;
-using CashFlow.Communication.Requests;
+﻿using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
-using System.Net.Http.Headers;
 
 namespace CashFlow.Application.UseCases.Expenses.Register
 {
@@ -17,29 +15,9 @@ namespace CashFlow.Application.UseCases.Expenses.Register
 
         private void Validate(RequestRegisterExpenseJson request)
         {
-            var titleIsEmpty = string.IsNullOrWhiteSpace(request.Title);
+            var validator = new RegisterExpenseValidator();
 
-            if (titleIsEmpty)
-            {
-                throw new ArgumentException("The title is required");
-            }
-
-            if (request.Amount <= 0)
-            {
-                throw new ArgumentException("The Amount must be greater than zero.");
-            }
-
-            var result = DateTime.Compare(request.Date, DateTime.UtcNow);
-            if (result < 0)
-            {
-                throw new ArgumentException("Expenses cannot be for the future");
-            }
-
-            var paymentTypeIsValid = Enum.IsDefined(typeof(PaymentType), request.PaymentType);
-            if (paymentTypeIsValid == false)
-            {
-                throw new ArgumentException("Payment Type is not valid.");
-            }
+            var result = validator.Validate(request);
         }
     }
 }
