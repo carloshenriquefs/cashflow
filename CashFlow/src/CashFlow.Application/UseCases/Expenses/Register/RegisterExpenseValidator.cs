2c6db5f9112450030ae87aa944ab637ab832640e
Carlos Henrique <ichfs9108@gmail.com>
2025-06-19 00:37:38 -0300

    localizando a nossa API

--- CashFlow/src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseValidator.cs
+++ CashFlow/src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseValidator.cs
@@ -13,6 +13,7 @@ namespace CashFlow.Application.UseCases.Expenses.Register
             RuleFor(expense => expense.Amount).GreaterThan(0).WithMessage(ResourceErrorMessages.AMOUNT_MUST_BE_GREATER_THAN_ZERO);
             RuleFor(expense => expense.Date).LessThanOrEqualTo(DateTime.UtcNow).WithMessage(ResourceErrorMessages.EXPENSES_CANNOT_FOR_THE_FUTURE);
             RuleFor(expense => expense.PaymentType).IsInEnum().WithMessage(ResourceErrorMessages.PAYMENT_TYPE_INVALID);
+
         }
     }
 }
